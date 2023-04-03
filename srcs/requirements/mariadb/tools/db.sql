5ed070606c2dc2e85b4092923daf7475e3909b13
Taha Namir <97416765+etahae@users.noreply.github.com>
2023-04-03 00:38:13 +0000

    Add files via upload

---
+++ srcs/requirements/mariadb/tools/db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE inception;
+GRANT all privileges on inception.* TO 'tnamir'@'localhost' identified by 'taha1337';
+flush privileges;
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'Taha1337';
