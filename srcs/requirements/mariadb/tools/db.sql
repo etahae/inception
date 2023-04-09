7011708b191b050ef9c3d2ffbec58ce75c52f31a
Taha Namir <97416765+etahae@users.noreply.github.com>
2023-04-09 15:20:13 +0000

    Delete srcs directory

--- srcs/requirements/mariadb/tools/db.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE inception;
-GRANT all privileges on inception.* TO 'tnamir'@'localhost' identified by 'taha1337';
-flush privileges;
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'Taha1337';
