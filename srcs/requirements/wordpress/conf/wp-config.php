08f29faa7feae3e5bf2f3b1bd96c3de7fdfb47ca
etahae <tahanamir@hotmail.com>
2023-04-12 11:50:57 +0000

    .

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -2,16 +2,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', '$MYSQL_DATABASE' );
 
 /** Database username */
-define( 'DB_USER', 'tnamir' );
+define( 'DB_USER', '$MYSQL_USER' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'user_password' );
+define( 'DB_PASSWORD', '$MYSQL_PASSWORD' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '$WORDPRESS_DB_HOST' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -52,7 +52,7 @@ define( 'NONCE_SALT',       'om.I&OsC>@gUa:|XKs-/vgZRuR7g`3M^> q[o&R3{i_Lly+h?A|
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix = 'wp_';
+$table_prefix = $WORDPRESS_TABLE_PREFIX;
 
 /**
  * For developers: WordPress debugging mode.
