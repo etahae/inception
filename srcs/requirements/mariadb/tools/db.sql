c7297ac81b1705e7124b65666e732e57c1678cab
etahae <tahanamir@hotmail.com>
2023-04-12 13:16:17 +0000

    .

--- srcs/requirements/mariadb/tools/db.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE wordpress;
-GRANT all privileges on wordpress.* TO 'tnamir'@'localhost' identified by 'user_password';
-flush privileges;
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'root_password';
