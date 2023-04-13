680e454bc029260aad55b2cacc25bc840cd86ae9
etahae <tahanamir@hotmail.com>
2023-04-13 09:32:25 +0000

    .

--- srcs/requirements/mariadb/tools/db.sql
+++ srcs/requirements/mariadb/tools/db.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE $MYSQL_DATABASE;
-GRANT all privileges on $MYSQL_DATABASE.* TO $MYSQL_USER@'localhost' identified by $MYSQL_PASSWORD;
+
+GRANT all privileges on $MYSQL_DATABASE.* TO $MYSQL_USER@'localhost' identified by userpassword;
+
 FLUSH privileges;
 
 ALTER USER $MYSQL_ROOT_USER@'localhost' IDENTIFIED BY $MYSQL_ROOT_PASSWORD;
