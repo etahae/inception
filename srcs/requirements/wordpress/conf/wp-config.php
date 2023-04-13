46910081e493eca1301fa644b60326062a776f7e
etahae <tahanamir@hotmail.com>
2023-04-13 22:03:05 +0000

    .

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -2,13 +2,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', $MYSQL_DATABASE );
+define( 'DB_NAME', $MARIADB_DATABASE );
 
 /** Database username */
-define( 'DB_USER', $MYSQL_USER );
+define( 'DB_USER', $MARIADB_USER );
 
 /** Database password */
-define( 'DB_PASSWORD', $MYSQL_PASSWORD );
+define( 'DB_PASSWORD', $MARIADB_PASSWORD );
 
 /** Database hostname */
 define( 'DB_HOST', $WORDPRESS_DB_HOST );
