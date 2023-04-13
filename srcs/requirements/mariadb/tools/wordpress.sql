bb0916e188b03de2ad6e2b08f8632d658b4cdfb4
etahae <tahanamir@hotmail.com>
2023-04-13 22:07:58 +0000

    .

--- srcs/requirements/mariadb/tools/wordpress.sql
+++ srcs/requirements/mariadb/tools/wordpress.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE '{$MARIADB_DATABASE}';
-CREATE USER '{$MARIADB_USER}'@localhost IDENTIFIED BY '{$MARIADB_PASSWORD}';
-GRANT all privileges ON '{$MARIADB_DATABASE}'.* TO '{$MARIADB_USER}'@localhost;
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER IF NOT EXISTS tnamir@localhost IDENTIFIED BY userpassword;
+GRANT all privileges ON wordpress.* TO tnamir@localhost;
 flush privileges;
-ALTER USER '{$MARIADB_ROOT_USER}'@localhost IDENTIFIED BY '{$MARIADB_ROOT_PASSWORD}';
+ALTER USER 'root'@localhost IDENTIFIED BY rootpassword;
