4d196b7526f25fbb743e6d6442a378656f3b45f7
etahae <tahanamir@hotmail.com>
2023-04-13 22:08:38 +0000

    .

--- srcs/requirements/mariadb/tools/wordpress.sql
+++ srcs/requirements/mariadb/tools/wordpress.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS tnamir@localhost IDENTIFIED BY userpassword;
+CREATE USER IF NOT EXISTS tnamir@localhost IDENTIFIED BY 'userpassword';
 GRANT all privileges ON wordpress.* TO tnamir@localhost;
 flush privileges;
 ALTER USER 'root'@localhost IDENTIFIED BY rootpassword;
