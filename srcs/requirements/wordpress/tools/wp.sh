08de28d55b4d4e9a92cef56750c43d7567c7f867
etahae <tahanamir@hotmail.com>
2023-04-14 13:24:03 +0000

    .

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -8,8 +8,8 @@ if [ ! -f /var/www/wordpress/index.php ]; then
     cp -r wordpress/ /var/www/
 
     cd /var/www/wordpress
-    sed -i "s/username_here/'wp_user'/g" wp-config-sample.php
-    sed -i "s/password_here/'userpassword'/g" wp-config-sample.php
+    sed -i "s/username_here/wp_user/g" wp-config-sample.php
+    sed -i "s/password_here/userpassword/g" wp-config-sample.php
     sed -i "s/localhost/'mariadb'/g" wp-config-sample.php
     sed -i "s/database_name_here/wordpress/g" wp-config-sample.php
     cp wp-config-sample.php wp-config.php
