2945fb0c0ba4aec04db220037c7b3ed6f33735cd
etahae <tahanamir@hotmail.com>
2023-04-14 20:33:56 +0000

    .

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -13,6 +13,4 @@ if [ ! -f /var/www/wordpress/index.php ]; then
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
-    wp core install --allow-root --url=tnamir.42.fr --title=inception --admin_user=tnamir --admin_password=rootpassword --admin_email=tnamir@gmail.com
-    wp user create --allow-root wp_user tnamir@hotmail.com --role=author --user_pass=userpassword
 fi
