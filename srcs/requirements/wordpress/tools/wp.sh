ca0644d0a98c8dbbe2177bd2352c6d1b5345820e
Taha Namir <tnamir@e2r2p13.1337.ma>
2023-04-15 10:42:48 +0000

    .

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -15,7 +15,7 @@ wp --allow-root core download --path=/var/www/html/wp
 
 # chown -R www-data:www-data /var/www/wordpress
 
-wp config create  --dbname=wordpress --dbuser=tnamir --dbpass=userpassword --dbhost=mariadb --path=/var/www/html/wp --allow-root
+wp config create  --dbname=wordpress --dbuser=tnamir --dbpass=userpassword --dbhost=mariadb:3306 --path=/var/www/html/wp --allow-root
 
 
 # wp core install --path=/var/www/wordpress --url=tnamir.42.fr --title=inception --admin_user=tnamir --admin_password=rootpassword --admin_email=tnamir@live.fr --skip-email --allow-root
