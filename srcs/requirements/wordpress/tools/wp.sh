3c7d0623b82ea01d3ef007f7373e95548f159cbb
Taha Namir <tnamir@e2r2p13.1337.ma>
2023-04-15 10:57:26 +0000

    .

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,26 +1,55 @@
 mkdir -p /run/php
 
-mkdir -p /var/www/
 
-# wget https://wordpress.org/latest.zip
-# unzip latest.zip
-# cp -r wordpress/ /var/www/
+wget https://wordpress.org/latest.zip
+unzip latest.zip
+cp -r wordpress/* /var/www/html/wp
 
-# mv /wp-config.php .
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+cd /var/www/html/wp
+sed -i "s/username_here/$DB_USER/g" wp-config-sample.php
+sed -i "s/password_here/$DB_USER_PASSWD/g" wp-config-sample.php
+sed -i "s/localhost/mariadb/g" wp-config-sample.php
+sed -i "s/database_name_here/$DB_NAME/g" wp-config-sample.php
+cp wp-config-sample.php wp-config.php
+
+chown -R www-data /var/www/wordpress
+
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-wp --allow-root core download --path=/var/www/html/wp
-
-# chown -R www-data:www-data /var/www/wordpress
-
-wp config create  --dbname=wordpress --dbuser=tnamir --dbpass=userpassword --dbhost=0.0.0.0 --path=/var/www/html/wp --allow-root
+wp core install --allow-root --url=tnamir.42.fr --title=inception --admin_user=root --admin_password=rootpassword --admin_email=root@gmail.com
+wp user create --allow-root tnamir tnamir@gmail.com --role=author --user_pass=userpassword
 
 
-# wp core install --path=/var/www/wordpress --url=tnamir.42.fr --title=inception --admin_user=tnamir --admin_password=rootpassword --admin_email=tnamir@live.fr --skip-email --allow-root
 
 
-# wp user create wp_user tnamir@hotmail.com --user_pass=userpassword --path=/var/www/wordpress --allow-root
 
-# ${MYSQL_DATABASE}
+
+
+
+
+
+
+# # wget https://wordpress.org/latest.zip
+# # unzip latest.zip
+# # cp -r wordpress/ /var/www/
+
+# # mv /wp-config.php .
+# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# chmod +x wp-cli.phar
+# mv wp-cli.phar /usr/local/bin/wp
+
+# wp --allow-root core download --path=/var/www/html/wp
+
+# # chown -R www-data:www-data /var/www/wordpress
+
+# wp config create  --dbname=wordpress --dbuser=tnamir --dbpass=userpassword --dbhost=localhost --path=/var/www/html/wp --allow-root
+
+
+# # wp core install --path=/var/www/wordpress --url=tnamir.42.fr --title=inception --admin_user=tnamir --admin_password=rootpassword --admin_email=tnamir@live.fr --skip-email --allow-root
+
+
+# # wp user create wp_user tnamir@hotmail.com --user_pass=userpassword --path=/var/www/wordpress --allow-root
+
+# # ${MYSQL_DATABASE}
