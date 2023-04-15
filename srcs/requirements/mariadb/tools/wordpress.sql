bc38370d2b26452e8165222a791d0adb1f26a946
Taha Namir <tnamir@e2r2p13.1337.ma>
2023-04-15 13:37:50 +0000

    .

--- srcs/requirements/mariadb/tools/wordpress.sql
+++ srcs/requirements/mariadb/tools/wordpress.sql
@@ -1,4 +1,3 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER IF NOT EXISTS 'tnamir'@'%' IDENTIFIED BY 'userpassword' ;
 GRANT ALL PRIVILEGES ON wordpress.* TO 'tnamir'@'%' ;
-FLUSH PRIVILEGES;
