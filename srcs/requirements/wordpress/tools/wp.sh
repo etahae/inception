e0d84ed04fdb6b8a12a8ac9892e8caff74f7bfda
etahae <tahanamir@hotmail.com>
2023-04-15 09:27:53 +0000

    .

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,28 +1,28 @@
 mkdir -p /run/php
 
-if [ ! -f /var/www/wordpress/index.php ]; then
-    mkdir -p /var/www/
+mkdir -p /var/www/
 
-    # wget https://wordpress.org/latest.zip
-    # unzip latest.zip
-    # cp -r wordpress/ /var/www/
+# wget https://wordpress.org/latest.zip
+# unzip latest.zip
+# cp -r wordpress/ /var/www/
 
-    # cd /var/www/wordpress
-    # mv /wp-config.php .
-    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-    chmod +x wp-cli.phar
-    mv wp-cli.phar /usr/local/bin/wp
-    wp --allow-root core download --path=/var/www/wordpress
+# mv /wp-config.php .
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
 
-    chown -R www-data:www-data /var/www/wordpress
+cd /var/www/http/wp
+echo "lol"
+# wp --allow-root core download --path=/var/www/wordpress
 
-    wp config create  --dbname=wordpress --dbuser=tnamir --dbpass=userpassword --dbhost=mariadb --path=/var/www/wordpress --allow-root
+# chown -R www-data:www-data /var/www/wordpress
 
-
-    # wp core install --path=/var/www/wordpress --url=tnamir.42.fr --title=inception --admin_user=tnamir --admin_password=rootpassword --admin_email=tnamir@live.fr --skip-email --allow-root
+# wp config create  --dbname=wordpress --dbuser=tnamir --dbpass=userpassword --dbhost=mariadb --path=/var/www/wordpress --allow-root
 
 
-    # wp user create wp_user tnamir@hotmail.com --user_pass=userpassword --path=/var/www/wordpress --allow-root
-fi
+# wp core install --path=/var/www/wordpress --url=tnamir.42.fr --title=inception --admin_user=tnamir --admin_password=rootpassword --admin_email=tnamir@live.fr --skip-email --allow-root
+
+
+# wp user create wp_user tnamir@hotmail.com --user_pass=userpassword --path=/var/www/wordpress --allow-root
 
 # ${MYSQL_DATABASE}
