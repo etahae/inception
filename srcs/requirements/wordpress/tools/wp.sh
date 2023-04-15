53c04e7fae80ad0b62f02bbea037f99a8865f802
etahae <tahanamir@hotmail.com>
2023-04-15 05:05:17 +0000

    .

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -7,13 +7,14 @@ if [ ! -f /var/www/wordpress/index.php ]; then
     # unzip latest.zip
     # cp -r wordpress/ /var/www/
 
-    cd /var/www/wordpress
+    # cd /var/www/wordpress
     # mv /wp-config.php .
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
     wp --allow-root core download --path=/var/www/wordpress
 
+    chown -R www-data:www-data /var/www/wordpress
 
     wp config create  --dbname=wordpress --dbuser=wp_user --dbpass=userpassword --dbhost=mariadb --path=/var/www/wordpress --allow-root
 
@@ -22,7 +23,6 @@ if [ ! -f /var/www/wordpress/index.php ]; then
 
 
     # wp user create wp_user tnamir@hotmail.com --user_pass=userpassword --path=/var/www/wordpress --allow-root
-    # chown -R www-data /var/www/wordpress
 fi
 
 # ${MYSQL_DATABASE}
