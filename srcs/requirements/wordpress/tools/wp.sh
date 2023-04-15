84abd5a969b0d2c8ad1a05217072991fe26178a9
Taha Namir <97416765+etahae@users.noreply.github.com>
2023-04-15 03:11:39 +0000

    Add files via upload

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -13,6 +13,16 @@ if [ ! -f /var/www/wordpress/index.php ]; then
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
-    wp core install --allow-root --url=tnamir.42.fr --title=inception --admin_user=tnamir --admin_password=rootpassword --admin_email=tnamir@gmail.com
-    wp user create --allow-root wp_user tnamir@hotmail.com --role=author --user_pass=userpassword
+    wp --allow-root core download --path=/var/www/wordpress
+
+
+    wp config create  --dbname=wordpress --dbuser=wp_user --dbpass=userpassword --dbhost=mariadb --path=/var/www/wordpress --allow-root
+
+
+    wp core install --path=/var/www/wordpress --url=tnamir.42.fr --title=inception --admin_user=tnamir --admin_password=rootpassword --admin_email=tnamir@live.fr --skip-email --allow-root
+
+
+    wp user create wp_user tnamir@hotmail.com --user_pass=userpassword --path=/var/www/wordpress --allow-root
 fi
+
+# ${MYSQL_DATABASE}
