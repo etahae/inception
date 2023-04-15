9bbf27ee0bb1503082a348fa9b5daff5cbb861d8
etahae <tahanamir@hotmail.com>
2023-04-15 09:06:39 +0000

    .

--- srcs/requirements/mariadb/tools/wordpress.sql
+++ srcs/requirements/mariadb/tools/wordpress.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER IF NOT EXISTS 'wp_user'@'%' IDENTIFIED BY 'userpassword' ;
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'%' ;
+ALTER USER 'tnamir'@'localhost' IDENTIFIED BY 'rootpassword';
 FLUSH PRIVILEGES;
