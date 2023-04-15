37561d49ac4c8b1a7be1c9a390d1111e1de2863f
etahae <tahanamir@hotmail.com>
2023-04-15 09:50:21 +0000

    .

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -7,7 +7,7 @@ mkdir -p /var/www/
 # cp -r wordpress/ /var/www/
 
 # mv /wp-config.php .
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
