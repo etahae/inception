959e905c1388a57e485584acafea0aaec7d9f9c2
etahae <tahanamir@hotmail.com>
2023-04-16 00:31:20 +0000

    .

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,6 +1,3 @@
-mkdir -p /run/php
-
-
 # wget https://wordpress.org/latest.zip
 # unzip latest.zip
 # cp -r wordpress/* /var/www/html/wp
@@ -22,18 +19,33 @@ mkdir -p /run/php
 # wp user create --allow-root tnamir tnamir@gmail.com --role=author --user_pass=userpassword
 
 
+#!/bin/sh
+
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
 chmod +x wp-cli.phar
+
 mv wp-cli.phar /usr/local/bin/wp
 
-wp --allow-root core download --path=/var/www/html/wp
+sed -i '36s/.*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
 
-wp config create  --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${DB_HOST} --path=/var/www/html/wp --allow-root
+mkdir -p /run/php
 
+cd /var/www/http/wp
 
-wp core install --path=/var/www/html/wp --url=${DOMAIN_NAME} --title=inception --admin_user=${MYSQL_ROOT_USER} --admin_password=${MYSQL_ROOT_PASSWORD} --admin_email=tnamir@live.fr --skip-email --allow-root
+wp --allow-root core download --path=/var/www/http/wp
 
+sleep 5;
 
-wp user create ${MYSQL_USER} tnamir@hotmail.com --user_pass=${MYSQL_PASSWORD} --path=/var/www/html/wp --allow-root
+wp config create  --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --path=/var/www/http/wp --allow-root
+
+sleep 3;
+
+wp core install --path=/var/www/http/wp --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMINE_USER} --admin_password=${WP_ADMINE_PASSWORD} --admin_email=${WP_ADMINE_EMAIL} --skip-email --allow-root
+
+sleep 3;
+
+wp user create ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --path=/var/www/http/wp --allow-root
+
 
 # ${MYSQL_DATABASE}
