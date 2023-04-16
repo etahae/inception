b011983343c0fa3578a7758685b242597f8f9013
etahae <tahanamir@hotmail.com>
2023-04-16 02:33:36 +0000

    .

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -31,21 +31,21 @@ sed -i '36s/.*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
 
 mkdir -p /run/php
 
-cd /var/www/http/wp
+cd /var/www/wordpress
 
-wp --allow-root core download --path=/var/www/http/wp
+wp --allow-root core download --path=/var/www/wordpress
 
 sleep 5;
 
-wp config create  --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --path=/var/www/http/wp --allow-root
+wp config create  --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --path=/var/www/wordpress --allow-root
 
 sleep 3;
 
-wp core install --path=/var/www/http/wp --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMINE_USER} --admin_password=${WP_ADMINE_PASSWORD} --admin_email=${WP_ADMINE_EMAIL} --skip-email --allow-root
+wp core install --path=/var/www/wordpress --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMINE_USER} --admin_password=${WP_ADMINE_PASSWORD} --admin_email=${WP_ADMINE_EMAIL} --skip-email --allow-root
 
 sleep 3;
 
-wp user create ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --path=/var/www/http/wp --allow-root
+wp user create ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --path=/var/www/wordpress --allow-root
 
 
 # ${MYSQL_DATABASE}
