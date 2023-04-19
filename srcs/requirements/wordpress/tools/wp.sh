b42faef4e0476d040a1878a1b11017c7e15d1aa3
etahae <tahanamir@hotmail.com>
2023-04-19 05:22:36 +0000

    .

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -36,6 +36,7 @@ cd ${WP_PATH}
 wp --allow-root core download --path=${WP_PATH}
 wp config create  --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${DB_HOST} --path=${WP_PATH} --allow-root
 wp core install --path=${WP_PATH} --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${ROOT_USER} --admin_password=${ROOT_PASSWORD} --admin_email=${WP_ROOT_EMAIL} --skip-email --allow-root
+sleep 5
 wp user create ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --path=${WP_PATH} --allow-root
 
 echo "wordpress is configured succefully!"
